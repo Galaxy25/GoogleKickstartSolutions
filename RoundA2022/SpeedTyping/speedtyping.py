81bd75e0d0ef36311331bd0369c264f5d0f772c4
Nathan St. John <84703098+Galaxy25@users.noreply.github.com>
2022-07-01 15:59:34 -0700

    SpeedTypingPython

---
+++ RoundA2022/SpeedTyping/speedtyping.py
@@ -0,0 +1,18 @@
+cases = int(input())
+for case in range(1, cases+1):
+    correct = input()
+    incorrect = input()
+    delete = 0
+    i = 0
+    u = 0
+    while u < len(incorrect) and i < len(correct):
+        if correct[i] == incorrect[u]:
+            i += 1
+            u += 1
+        else:
+            u += 1
+            delete += 1
+    if len(correct) != i:
+        print(f"Case #{case}: IMPOSSIBLE")
+    else:
+        print(f"Case #{case}: {delete + (len(incorrect) - u)}")
