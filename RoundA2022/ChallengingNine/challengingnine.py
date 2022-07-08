9083b1b7d4d634a03caace78785ee88a853693f8
Nathan St. John <84703098+Galaxy25@users.noreply.github.com>
2022-07-08 15:46:13 -0700

    ChallengeNine Works

--- RoundA2022/ChallengingNine/challengingnine.py
+++ RoundA2022/ChallengingNine/challengingnine.py
@@ -3,17 +3,16 @@ cases = input()
 for i in range(1, int(cases) + 1):
     number = input()
     divisor = 9 - (int(number) % 9)
-    number = number[::-1]
     u = 0
     end = True
     if divisor == 9:
-        number = "0" + number
+        number = number[0] + "0" + number[1:]
         end = False
     while u < len(number) and end:
-        if divisor >= int(number[u]):
+        if divisor < int(number[u]):
             number = number[:u] + str(divisor) + number[u:]
             end = False
         u += 1
     if end:
         number = number + str(divisor)
-    print(f"Case #{i}: {int(number[::-1])}")
+    print(f"Case #{i}: {int(number)}")
